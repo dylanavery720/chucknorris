805974b1abb4bfb4ff730724ae0c1f9b496abebc
Dylan Avery <fossilfuelmusic@gmail.com>
2017-01-27 08:54:04 -0700

    no favorites styling

--- app/components/Favorites/Favorites.js
+++ app/components/Favorites/Favorites.js
@@ -7,7 +7,7 @@ export default class Favorites extends React.Component {
   render() {
     return (
     <div className="chuck-jokes">
-    {this.props.favorites.length < 1 && <h1>YOU HAVE NO FAVORITES</h1>}
+    {this.props.favorites.length < 1 && <h1 className="instructions">YOU HAVE NO FAVORITES</h1>}
     <ul>
     {this.props.favorites.map((joke, i) => <li className="a-joke" key={i} id={i}>{joke}<Button clas="fave-button" text="&#9733;" handleClick={() => this.props.handleCheck(joke, i)} /></li>)}
     </ul>
